b9be0b349a837a5fb01c8d41945d6639fcbce1b4
Giovanni Maschietto <giovanni.maschietto@h-farm.com>
2026-09-11 13:59:53 +0200

    Change workflows arch

--- scaffold/app-web/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-
-function App() {
-  return <h1>&lt;nome-app&gt;</h1>
-}
-
-createRoot(document.getElementById('root')).render(<App />)
--- scaffold/service-py/conftest.py
+++
@@ -1,5 +0,0 @@
-# Rende importabile `app` dai test senza installare il servizio come pacchetto.
-import sys
-from pathlib import Path
-
-sys.path.insert(0, str(Path(__file__).parent))
